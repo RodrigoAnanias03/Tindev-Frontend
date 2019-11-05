d0befa2eced261dc8c77f0692ef739405f0024fb
RodrigoAnanias03 <r.ananias03@gmail.com>
2019-11-05 07:53:27 -0300

    Config first form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg'
+
 function App() {
   return (
-    
+    <div className="container">
+      <img src={logo} alt=""/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots </strong> para programadores e encontre <strong>talentos </strong> para sua empresa
+          
+          <form>
+            <label htmlFor="email">E-MAIL *</label>
+            <input 
+              type="email"
+              id="emal"
+              placeholder="Seu melhor e-mail"
+            />
+
+            <button className="btn" type="submit">Entrar</button>
+          </form>
+        </p>
+      </div>
+
+    </div>
   );
 }
 
